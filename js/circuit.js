c17457c53a3e726137f9adac151bf30875325793
Michael N. Gagnon <mng@Michaels-MacBook-Pro.local>
2021-08-27 09:16:16 -0400

    .

--- js/circuit.js
+++ js/circuit.js
@@ -7,89 +7,91 @@
   }
 }*/
 
-const TRADIUS = 200;
+const TRADIUS = 50;
 
 class TransistorGraphic {
 
     // Absolute x y coordinates
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
+    constructor(strokeWidth) {
+        this.x = 0;
+        this.y = 0;
+        this.strokeWidth = strokeWidth;
         this.width = TRADIUS * 2;
         this.height = TRADIUS * 2;
         this.radius = TRADIUS;
+        this.draw();
     }
 
-    draw(stage, strokeWidth) {
+    draw() {
+
+        this.container = new createjs.Container();
+
         const circle = new createjs.Shape();
         circle
             .graphics
-            .setStrokeStyle(strokeWidth)
+            .setStrokeStyle(this.strokeWidth)
             .beginStroke("black")
             .drawCircle(this.radius, this.radius, this.radius);
         circle.x = this.x;
         circle.y = this.y;
-        stage.addChild(circle);
+        this.container.addChild(circle);
 
         const bar = new createjs.Shape();
         bar
             .graphics
-            .setStrokeStyle(strokeWidth)
+            .setStrokeStyle(this.strokeWidth)
             .beginStroke("black")
             .moveTo(this.x + Math.floor(TRADIUS / 1.7), this.y + Math.floor(TRADIUS / 1.7))
             .lineTo(this.x + Math.floor(TRADIUS / 1.7), this.y + Math.floor(TRADIUS / 1.7 * 2.5))
             .endStroke();
-        stage.addChild(bar);
+        this.container.addChild(bar);
 
         const left = new createjs.Shape();
         left
             .graphics
-            .setStrokeStyle(strokeWidth)
+            .setStrokeStyle(this.strokeWidth)
             .beginStroke("black")
             .moveTo(this.x, this.y + TRADIUS)
             .lineTo(this.x + Math.floor(TRADIUS / 1.7), this.y + TRADIUS)
             .endStroke();
-        stage.addChild(left);
+        this.container.addChild(left);
 
         const top = new createjs.Shape();
         top
             .graphics
-            .setStrokeStyle(strokeWidth)
+            .setStrokeStyle(this.strokeWidth)
             .beginStroke("black")
             .moveTo(this.x + Math.floor(TRADIUS / 1.7), this.y + Math.floor(TRADIUS / 1.7) + Math.floor(TRADIUS / 5))
             .lineTo(this.x + TRADIUS * 2, this.y)
             .endStroke();
-        stage.addChild(top);
+        this.container.addChild(top);
 
         const bottom = new createjs.Shape();
         bottom
             .graphics
-            .setStrokeStyle(strokeWidth)
+            .setStrokeStyle(this.strokeWidth)
             .beginStroke("black")
             .moveTo(this.x +  Math.floor(TRADIUS / 1.7), this.y + TRADIUS * 2 - (Math.floor(TRADIUS / 1.7) + Math.floor(TRADIUS / 5)))
             .lineTo(this.x + TRADIUS * 2, this.y + TRADIUS * 2)
             .endStroke();
-        stage.addChild(bottom);
+        this.container.addChild(bottom);
 
         const arrow = new createjs.Shape();
         const size = TRADIUS / 3;
         const sides = 3;
         const angle = 30;
         arrow.graphics.beginFill("black").drawPolyStar(this.x + TRADIUS * 1.3, this.y + TRADIUS * 1.6, size, sides, 0, angle);
-        // arrow
-        //     .graphics
-        //     //.setStrokeStyle(strokeWidth)
-        //     //.beginStroke("black")
-        //     .beginPath()
-        //     .moveTo(20, 20)
-        //     .lineTo(30, 30)
-        //     .lineTo(25, 25)
-        //     .lineTo(20, 20)
-        //     .closePath()
-        //     .fillStyle("blue")
-        //     .fill()
-        //     //.endStroke();
-        stage.addChild(arrow);
+        this.container.addChild(arrow);
+
+        /*const clone = this.container.clone(true);
+        clone.x = 100;
+        clone.y = 100;
+
+        stage.addChild(this.container);
+        stage.addChild(clone);
+
+        this.container.x = 50;
+        this.container.y = 50;*/
     }
 }
 
@@ -100,18 +102,26 @@ class Circuit {
     this.canvasId = canvasId;
     this.strokeWidth = strokeWidth;
     this.stage = new createjs.Stage(canvasId);
+
+    this.transistorGraphic = new TransistorGraphic(this.strokeWidth)
     //this.stage.scale = 1/2;
   }
 
-  addItem(item) {
-    item.draw(this.stage, this.strokeWidth);
-  }
+  /*addItem(item) {
+    item.draw(this.strokeWidth);
+  }*/
 }
 
 function main() {
     const STROKE_WIDTH = 1;
     const circuit1 = new Circuit(500, 300, "circuit-canvas-1", STROKE_WIDTH);
-    const t1 = new TransistorGraphic(10, 10);
-    circuit1.addItem(t1);
+    //const t1 = new TransistorGraphic(10, 10);
+    //circuit1.addItem(t1);
+
+    const t = circuit1.transistorGraphic.container.clone(true);
+    t.x = 100;
+    t.y = 100;
+    circuit1.stage.addChild(t);
+
     circuit1.stage.update();
 }
