06be44a6a64e5bf3784ad71133795538a060d2dd
Michael N. Gagnon <mng@Michaels-MacBook-Pro.local>
2021-08-27 13:55:23 -0400

    .

--- js/circuit.js
+++ js/circuit.js
@@ -6,16 +6,17 @@
     this.y = y;
   }
 }*/
-const TRADIUS = 100;
-const STROKE_WIDTH = 1;
+const TRADIUS = 40;
+const STROKE_WIDTH = 2;
 const DASH = 6;
 
 class NotGate {
-    constructor(tradius, strokeWidth, transistorGraphic, resistorGraphic) {
+    constructor(tradius, strokeWidth, transistorGraphic, resistorGraphic, groundGraphic) {
         this.tradius = tradius;
         this.strokeWidth = strokeWidth;
         this.transistorGraphic = transistorGraphic;
         this.resistorGraphic = resistorGraphic;
+        this.groundGraphic = groundGraphic;
         this.width = this.tradius * 8;
         this.height = this.tradius * 8;
         this.plusStrokeWidth = Math.floor(this.tradius / 7);
@@ -107,16 +108,65 @@ class NotGate {
             .setStrokeStyle(this.strokeWidth)
             .beginStroke("black")
             .moveTo(Math.floor(this.width / 2) - this.tradius, Math.floor(this.height / 2))
-            .lineTo(Math.floor(this.width / 2) - this.tradius - this.tradius * 0.2, Math.floor(this.height / 2))
+            .lineTo(Math.floor(this.width / 2) - this.tradius - Math.floor(this.tradius * 0.2), Math.floor(this.height / 2))
             .endStroke();
         this.container.addChild(inputWire1);
 
         const r2 = this.resistorGraphic.container.clone(true);
+        r2.regX = this.resistorGraphic.width / 2;
+        r2.regY = this.resistorGraphic.height / 2;
         r2.rotation = 90
+        r2.x = this.resistorGraphic.height / 2;
+        r2.y = this.resistorGraphic.width / 2;
 
-        r2.x = Math.floor(this.width / 2) - this.tradius - this.tradius * 0.2 - this.resistorGraphic.width;
-        r2.y = Math.floor(this.height / 2) - this.tradius * 1.2 - this.resistorGraphic.height;
-        this.container.addChild(r2);
+        const r2wrap = new createjs.Container();
+        r2wrap.addChild(r2);
+
+
+        r2wrap.x = Math.floor(this.width / 2) - this.tradius - this.resistorGraphic.height - Math.floor(this.tradius * 0.2);
+        r2wrap.y = Math.floor(this.height / 2) - Math.floor(this.resistorGraphic.width / 2);
+
+        this.container.addChild(r2wrap);
+
+        const inputWire2 = new createjs.Shape();
+        inputWire2
+            .graphics
+            .setStrokeStyle(this.strokeWidth)
+            .beginStroke("black")
+            .moveTo(0, Math.floor(this.height / 2))
+            .lineTo(r2wrap.x, Math.floor(this.height / 2))
+            .endStroke();
+        this.container.addChild(inputWire2);
+
+        const g = this.groundGraphic.container.clone(true);
+        g.x = Math.floor(this.width / 2) + this.tradius - Math.floor(this.groundGraphic.width / 2);
+        g.y = Math.floor(this.height / 2) + this.tradius * 2;
+        this.container.addChild(g);
+
+
+
+        const gwire = new createjs.Shape();
+        gwire
+            .graphics
+            .setStrokeStyle(this.strokeWidth)
+            .beginStroke("black")
+            .moveTo(t.x + this.tradius * 2, t.y + this.tradius * 2)
+            .lineTo(t.x + this.tradius * 2, Math.floor(this.height / 2) + this.tradius * 2)
+            .endStroke();
+        this.container.addChild(gwire);
+
+
+
+        const outwire = new createjs.Shape();
+        outwire
+            .graphics
+            .setStrokeStyle(this.strokeWidth)
+            .beginStroke("black")
+            .moveTo(Math.floor(this.width / 2) + this.tradius, Math.floor(this.height / 2) - this.tradius)
+            .lineTo(Math.floor(this.width / 2) + this.tradius + this.tradius, Math.floor(this.height / 2) )
+            .lineTo(this.width, Math.floor(this.height / 2) )
+            .endStroke();
+        this.container.addChild(outwire);
 
         /*this.container.regX = this.width / 2;
         this.container.regY = this.width / 2;
@@ -138,6 +188,72 @@ class NotGate {
             .lineTo(this.tradius * 3, 0)
             .endStroke();
         this.container.addChild(plusVert);*/
+    }
+}
+
+class GroundGraphic {
+
+    constructor(tradius, strokeWidth) {
+        this.tradius = tradius;
+        this.strokeWidth = strokeWidth;
+        this.draw();
+    }
+
+    draw() {
+        const stepwidth = Math.floor(this.tradius / 7);
+        const gapheight = Math.floor(this.tradius / 7);
+
+        this.stepwidth = stepwidth;
+        this.gapheight = gapheight;
+        this.width = stepwidth * 6;
+        this.height = gapheight * 3;
+        this.centerx = Math.floor(this.width / 2);
+        const centerx = this.centerx
+
+        this.container = new createjs.Container();
+
+
+        /*const dash = DASH;
+        const outline = new createjs.Shape();
+        outline
+            .graphics
+            .beginStroke("gray")
+            .setStrokeStyle(this.strokeWidth)
+            .setStrokeDash([dash, dash], 0)
+            .drawRect(0, 0, this.width, this.height);
+        this.container.addChild(outline);*/
+        
+
+
+        const z1 = new createjs.Shape();
+        z1
+            .graphics
+            .setStrokeStyle(this.strokeWidth)
+            .beginStroke("black")
+            .moveTo(0, 0)
+            .lineTo(this.width, 0)
+            .endStroke();
+        this.container.addChild(z1);
+
+        const z2 = new createjs.Shape();
+        z2
+            .graphics
+            .setStrokeStyle(this.strokeWidth)
+            .beginStroke("black")
+            .moveTo(this.stepwidth, this.gapheight)
+            .lineTo(this.width - this.stepwidth, this.gapheight)
+            .endStroke();
+        this.container.addChild(z2);
+
+        const z3 = new createjs.Shape();
+        z3
+            .graphics
+            .setStrokeStyle(this.strokeWidth)
+            .beginStroke("black")
+            .moveTo(this.stepwidth * 2 , this.gapheight * 2)
+            .lineTo(this.width - this.stepwidth * 2, this.gapheight * 2)
+            .endStroke();
+        this.container.addChild(z3);
     }
 }
 
@@ -292,7 +408,8 @@ class Circuit {
 
     this.transistorGraphic = new TransistorGraphic(this.tradius, this.strokeWidth);
     this.resistorGraphic = new ResistorGraphic(this.tradius, this.strokeWidth);
-    this.notGate = new NotGate(this.tradius, this.strokeWidth, this.transistorGraphic, this.resistorGraphic);
+    this.groundGraphic = new GroundGraphic(this.tradius, this.strokeWidth);
+    this.notGate = new NotGate(this.tradius, this.strokeWidth, this.transistorGraphic, this.resistorGraphic, this.groundGraphic);
     //this.stage.scale = 1/2;
   }
 
