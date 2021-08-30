8c577e2e95aaf3d1123136f961dbada580bcd4be
Michael N. Gagnon <mng@Michaels-MacBook-Pro.local>
2021-08-29 22:58:44 -0400

    .

--- js/circuit.js
+++ js/circuit.js
@@ -1453,7 +1453,37 @@ class Circuit {
 }
 
 class FullAdder {
-    setInput(value1, value2) {   
+    setInput(y, x, c) {
+        this.inputY = y;
+        this.inputX = x;
+        this.inputC = c;
+
+        this.leftHalf.setInput(this.inputY, this.inputX);
+        this.rightHalf.setInput(this.leftHalf.output2, this.inputC);
+        this.orGate.setInput(this.leftHalf.output1, this.rightHalf.output1);
+
+        this.outputC = this.orGate.getOutput();
+        this.outputZ = this.rightHalf.output2;
+
+        if (this.inputY) {
+            this.yinLight.fillCommand.style = LIGHT_ON_COLOR;
+        } else {
+            this.yinLight.fillCommand.style = LIGHT_OFF_COLOR;
+        }
+
+        if (this.inputX) {
+            this.xinLight.fillCommand.style = LIGHT_ON_COLOR;
+        } else {
+            this.xinLight.fillCommand.style = LIGHT_OFF_COLOR;
+        }
+
+        if (this.inputC) {
+            this.cinLight.fillCommand.style = LIGHT_ON_COLOR;
+        } else {
+            this.cinLight.fillCommand.style = LIGHT_OFF_COLOR;
+        }
+
+
     }
 
     constructor(stage, tradius, fullAdderStrokeWidth, halfAdderStrokeWidth, primStrokeWidth, subStrokeWidth) {
@@ -1707,7 +1737,10 @@ class FullAdder {
         this.container.addChild(cinwireBulb);
 
 
-        this.cinLight = new Light(this.tradius, this.fullAdderStrokeWidth, this.bulbSize);
+        this.cinLight = new Light(this.tradius, this.fullAdderStrokeWidth, this.bulbSize, function(){
+            THIS.setInput(THIS.inputY, THIS.inputX, !THIS.inputC);
+            THIS.stage.update();
+        });
         this.cinLight.container.x = rh.x + this.rightHalf.inRightX + this.bulbSize * this.tradius ;
         this.cinLight.container.y = lh.y - this.bulbSize * this.tradius * 3;
         this.container.addChild(this.cinLight.container);
@@ -1756,7 +1789,10 @@ class FullAdder {
         this.container.addChild(xinwireBulb);
 
 
-        this.xinLight = new Light(this.tradius, this.fullAdderStrokeWidth, this.bulbSize);
+        this.xinLight = new Light(this.tradius, this.fullAdderStrokeWidth, this.bulbSize, function(){
+            THIS.setInput(THIS.inputY, !THIS.inputX, THIS.inputC);
+            THIS.stage.update();
+        });
         this.xinLight.container.x = lh.x + this.leftHalf.inRightX + this.bulbSize * this.tradius ;
         this.xinLight.container.y = lh.y + this.leftHalf.height + this.bulbSize * this.tradius * 1;
         this.container.addChild(this.xinLight.container);
@@ -1773,7 +1809,10 @@ class FullAdder {
         this.container.addChild(yinwireBulb);
 
 
-        this.yinLight = new Light(this.tradius, this.fullAdderStrokeWidth, this.bulbSize);
+        this.yinLight = new Light(this.tradius, this.fullAdderStrokeWidth, this.bulbSize, function(){
+            THIS.setInput(!THIS.inputY, THIS.inputX, THIS.inputC);
+            THIS.stage.update();
+        });
         this.yinLight.container.x = lh.x + this.leftHalf.inLeftX + this.bulbSize * this.tradius ;
         this.yinLight.container.y = lh.y + this.leftHalf.height + this.bulbSize * this.tradius * 1;
         this.container.addChild(this.yinLight.container);
